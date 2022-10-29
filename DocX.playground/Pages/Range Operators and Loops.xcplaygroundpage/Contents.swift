aaf4519635d8dd7b2021a8a506aecf8fc7e5780c
jaroslav lzicar <lzicar@icloud.com>
2022-10-29 23:18:49 +0200

    sync

---
+++ DocX.playground/Pages/Range Operators and Loops.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+
+
+import UIKit
+
+let myRange = 10...20
+let myRange2 = 10..<20
+
+for number in myRange2 {
+    print(number)
+}
+
+for number in (0...5).reversed() {
+    print(number)
+}
+
+var y = 0
+while y < 50 {
+    y += 5
+    print("y is \(y)")
+}
+
+var x = 100
+repeat {
+    x += 5
+    print("x is \(x)")
+} while x < 50
+            
+            
